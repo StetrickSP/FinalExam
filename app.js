668afd7265497f0be11880c1c76591741f44f5d4
StetrickSP <stetricksp@gmail.com>
2023-11-05 10:55:23 +0700

    commit

--- app.js
+++ app.js
@@ -8,8 +8,28 @@ window.onscroll = () =>
 {
     searchForm.classList.remove('active');
 }
+
 $(document).ready(function()
 {
-    //owl Carousel
-    
-})
+    //Owl Carousel
+    $('.owl-carousel').owlCarousel({
+        margin: 5,
+        nav: true,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoHoverPause: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{  
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }   
+    });
+});
+
